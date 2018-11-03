58c25647ec71146fcb1a0a96f3808e5eaaa1723f
weihu <1183969348@qq.com>
2018-11-03 21:37:27 +0800

    更新 song.html 页面显示逻辑

--- song.js
+++ song.js
@@ -1,8 +1,39 @@
 $(function () {
-    $.get('/lyric.json').then(function (object) {
-        let {
-            lyric
-        } = object
+    
+    let id = parseInt(location.search.match(/\bid=([^&]*)/)[1])
+
+    $.get('./songs.json').then(function(response){
+        let songs = response
+        let song =  songs.filter(s=>s.id === id)[0]
+        let {url,name,lyric} = song
+        
+        initPlayer.call(undefined,url)
+        initText(name,lyric)
+    })
+    function initText(name,lyric){
+        $('.song-description > h1').text(name)
+        parseLyric(lyric)
+    }
+    function initPlayer(url){
+        let audio = document.createElement('audio')
+        audio.src = url
+        audio.oncanplay = function(){
+            audio.play()
+            $('.disc-container').addClass('playing')
+        }
+        $('.icon-pause').on('touchstart', function(){
+            audio.pause()
+            $('.disc-container').removeClass('playing')        
+        })
+        $('.icon-play').on('touchstart', function(){
+            audio.play()
+            $('.disc-container').addClass('playing')        
+        })
+    }
+
+
+   
+    function parseLyric(lyric){
         let array = lyric.split('\n')
         let regex = /^\[(.+)\](.*)$/
         array = array.map(function (string, index) {
@@ -24,19 +55,6 @@ $(function () {
             $p.attr('data-time', object.time).text(object.words)
             $p.appendTo($lyric.children('.lines'))
         })
-    })
-    let audio = document.createElement('audio')
-    audio.src = '//pgptqkwq1.bkt.clouddn.com/C400001XdWPc3zSK8E.m4a'
-    audio.oncanplay = function(){
-        audio.play()
-        $('.disc-container').addClass('playing')
     }
-    $('.icon-pause').on('touchstart', function(){
-        audio.pause()
-        $('.disc-container').removeClass('playing')        
-    })
-    $('.icon-play').on('touchstart', function(){
-        audio.play()
-        $('.disc-container').addClass('playing')        
-    })
+ 
 })
